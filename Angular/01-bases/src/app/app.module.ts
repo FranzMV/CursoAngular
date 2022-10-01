9256c6029167c46eaff23daa039df0853748d9bc
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 13:16:06 +0200

    01-Bases Angular: Componente Heroe y separacion de directorios

--- Angular/01-bases/src/app/app.module.ts
+++ Angular/01-bases/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
