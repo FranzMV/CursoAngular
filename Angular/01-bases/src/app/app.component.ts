c84d45a481f283959144277ee95d6fe1b51b64b2
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 13:00:08 +0200

    01-Bases Angular: Crear un componente manualmente

--- Angular/01-bases/src/app/app.component.ts
+++ Angular/01-bases/src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-  base : number = 5;
-
-  acumular(valor : number){
-    this.numero += valor;
-  }
+  
 }
