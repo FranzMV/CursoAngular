91b311fde679be6727f423bd9f1d1f07d7aa9d09
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 13:42:05 +0200

    01-Bases Angular: Cambios en el template del componente

--- Angular/01-bases/src/app/heroes/heroe/heroe.component.ts
+++ Angular/01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 })
 
 export class HeroeComponent{
+    nombre: string = 'Ironman';
+    edad: number = 45;
 
+    get nombreCapitalizado(){
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre() :string{
+        return `${this.nombre} - ${this.edad}`;
+    }
 }
