33613d7d01bfdaea5d44cb5161b4a1bf05c39e58
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 13:49:11 +0200

    01-Bases Angular: Concepto de one-way-databinding: enlazado en una sola via

--- Angular/01-bases/src/app/heroes/heroe/heroe.component.ts
+++ Angular/01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -10,11 +10,19 @@ export class HeroeComponent{
     nombre: string = 'Ironman';
     edad: number = 45;
 
-    get nombreCapitalizado(){
+    get nombreCapitalizado() : string {
         return this.nombre.toUpperCase();
     }
 
-    obtenerNombre() :string{
+    obtenerNombre() :string {
         return `${this.nombre} - ${this.edad}`;
+    }
+
+    cambiarNombre() :void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad() :void {
+        this.edad = 30;
     }
 }
