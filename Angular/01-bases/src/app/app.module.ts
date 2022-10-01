8e057f857827e1a993756aa788382c20929f844b
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 15:30:12 +0200

    01-Bases Angular: Creación módulo contador

--- Angular/01-bases/src/app/app.module.ts
+++ Angular/01-bases/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { HeroeModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HeroeModule
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
