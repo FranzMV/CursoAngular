add8612054fc42b7d81d5e17a270c5f612f6f95d
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 12:45:52 +0200

    01-Bases Angular: Metodos en el componente

--- Angular/01-bases/src/app/app.component.ts
+++ Angular/01-bases/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string = 'Contador App';
   numero: number = 10;
+
+  acumular(valor : number){
+    this.numero += valor;
+  }
+
+  
 }
