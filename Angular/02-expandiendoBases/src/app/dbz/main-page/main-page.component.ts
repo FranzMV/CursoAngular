2d98bbec0f6af3cb8cf7d3bce3120a4abf969225
FranMV <FranManzanedoValle@gmail.com>
2022-10-01 23:47:46 +0200

    02-Expandiendo las bases: Mostrar listado de personajes

--- Angular/02-expandiendoBases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/02-expandiendoBases/src/app/dbz/main-page/main-page.component.ts
@@ -11,16 +11,31 @@ interface Personaje{
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
   
   nuevo : Personaje ={
-    nombre: 'Trucks',
-    poder: 14000
+    nombre: '',
+    poder: 0
   }
 
-
   agregar(){
+    if(this.nuevo.nombre.trim().length === 0) { return };
     console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
   }
 
 }
