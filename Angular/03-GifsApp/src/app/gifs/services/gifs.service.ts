e97620dbba57f09210a9dee77674415544df6d47
FranMV <FranManzanedoValle@gmail.com>
2022-10-02 22:46:04 +0200

    03-GifsApp: HttpParams

--- Angular/03-GifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/03-GifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 
@@ -10,36 +10,44 @@ import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 })
 export class GifsService {
 
-  private apiKey     : string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q';
+  private apiKey     : string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5';
   private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
-  private limit : number = 10;
-
   public resultados : Gif [] = [];
-
   private _historial : string [] = [];
 
   get historial(){ return [...this._historial]; }
 
   constructor(private http: HttpClient){ 
-
-    if( localStorage.getItem('historial')){
-      this._historial = JSON.parse(localStorage.getItem('historial')!);
-    }
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    // if( localStorage.getItem('historial')){
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+    // if( localStorage.getItem('resultados')){
+    //   this._historial = JSON.parse(localStorage.getItem('resultados')!);
+    // }
   }
 
-  buscarGifs( query: string ) : void{
-    query = query.trim().toLowerCase();
+  buscarGifs( query: string = '' ) : void{
+    query = query.trim().toLocaleLowerCase();
     if(!this._historial.includes( query )){
       this._historial.unshift( query );
       this._historial = this._historial.splice(0, 10);
 
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', query );
+
     //Peticion Http
-    this.http.get <SearchGifsResponse>(`${this.servicioUrl}/search?api_key=${this.apiKey}=${query}&limit=${this.limit}`)
+    this.http.get <SearchGifsResponse>(`${this.servicioUrl}/search`,{ params })
       .subscribe( (resp ) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 
--- Angular/03-GifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ Angular/03-GifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -4,14 +4,18 @@ import { GifsService } from '../../gifs/services/gifs.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styles: [
+  ]
 })
 export class SidebarComponent {
 
-  get historial(){
-    return this.gifService.historial;
-  }
-
+  get historial(){ return this.gifService.historial; }
   //Injectamos el servicio
   constructor(private gifService: GifsService) { }
+
+  buscar(termino : string) : void {
+    this.gifService.buscarGifs(termino);
+
+  }
 
 }
