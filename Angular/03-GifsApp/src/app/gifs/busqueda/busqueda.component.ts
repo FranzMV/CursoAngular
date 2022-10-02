5793c0815ff66610a9436364e83c8e0309280fd9
FranMV <FranManzanedoValle@gmail.com>
2022-10-02 15:18:29 +0200

    03-GifsApp: GifsModule y sus componentes

---
+++ Angular/03-GifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
