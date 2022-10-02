51d1b7161e50c02813cfaa5220379832cc584d81
FranMV <FranManzanedoValle@gmail.com>
2022-10-02 20:39:30 +0200

    03-GifsApp: Peticion Http

--- Angular/03-GifsApp/src/app/app.module.ts
+++ Angular/03-GifsApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -11,8 +12,10 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/03-GifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/03-GifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
 
 @Injectable({
   providedIn: 'root'//Se encuentra a nivel global
 })
 export class GifsService {
 
-  constructor() { }
+  private apiKey     : string = 'CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
 
   private _historial : string [] = [];
 
-  get historial(){
-    return [...this._historial];
-  }
+  get historial(){ return [...this._historial]; }
 
-  buscarGifs( query: string ){
-    
+  constructor(private http: HttpClient){ }
+
+  buscarGifs( query: string ) : void{
     query = query.trim().toLowerCase();
-
     if(!this._historial.includes( query )){
       this._historial.unshift( query );
+      this._historial = this._historial.splice(0, 10);
     }
-    this._historial = this._historial.splice(0, 10);
+    
     console.log(this._historial);
 
+    //Peticion Http
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=CtbSNZMFPFlbWl1x5KQjtqPOHnE1ema5&q=dragon ball z&limit=10')
+      .subscribe( (resp: any ) => {
+        console.log(resp.data);
+      });
   }
+
+  
 }
