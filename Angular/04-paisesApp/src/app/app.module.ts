d44a97e52bbc90d108dd460da9d31c3c63cb8670
FranMV <FranManzanedoValle@gmail.com>
2022-10-09 11:40:26 +0200

    04-PaisesApp: Creando los modulos y componentes basicos

---
+++ Angular/04-paisesApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PaisModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
