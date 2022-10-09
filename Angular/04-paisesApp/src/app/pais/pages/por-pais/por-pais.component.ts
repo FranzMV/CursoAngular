7b93d1d92f4ec1ec0bf25df119e67aad67ff9a3d
FranMV <FranManzanedoValle@gmail.com>
2022-10-09 18:19:18 +0200

    04-PaisesApp: Llenar la tabla de paises

--- Angular/04-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ Angular/04-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,8 +8,10 @@ import { PaisService } from '../../services/pais.service';
   styleUrls: ['./por-pais.component.css']
 })
 export class PorPaisComponent {
+  
   termino : string = ' ';
   hayError : boolean = false;
+  paises : Country [] = [];
 
   constructor( private paisSevice: PaisService) { }
 
@@ -20,11 +23,13 @@ export class PorPaisComponent {
         {
           next:(paises) =>{
             console.log(paises);
+            this.paises = paises;
           },
           error: (err) => {
             console.log('Error');
             console.info(err);
             this.hayError = true;
+            this.paises = [];
           }
         }
         );
--- Angular/04-paisesApp/src/app/pais/pais.module.ts
+++ Angular/04-paisesApp/src/app/pais/pais.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
+
 
 
 
@@ -25,7 +28,8 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
