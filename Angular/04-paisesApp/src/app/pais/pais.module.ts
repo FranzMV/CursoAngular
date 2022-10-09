c3990fd660ea10991b5eaccf38dcd90a8c8ef8d3
FranMV <FranManzanedoValle@gmail.com>
2022-10-09 17:01:27 +0200

    04-PaisesApp: Componentes para buscar por país

--- Angular/04-paisesApp/src/app/pais/pais.module.ts
+++ Angular/04-paisesApp/src/app/pais/pais.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
+
 
 
 
@@ -21,7 +24,8 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class PaisModule { }
--- Angular/04-paisesApp/src/app/shared/sidebar/sidebar.component.ts
+++ Angular/04-paisesApp/src/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  styles:[
+    `
+    li{
+      cursor: pointer;
+    }
+    `
+  ]
 })
 export class SidebarComponent implements OnInit {
 
