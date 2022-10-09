7fd59a5ab88f105500ebe34c11bf51adbdd1c073
FranMV <FranManzanedoValle@gmail.com>
2022-10-09 12:47:28 +0200

    04-PaisesApp: Componente Sidebar

--- Angular/04-paisesApp/src/app/app-routing.module.ts
+++ Angular/04-paisesApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
--- Angular/04-paisesApp/src/app/shared/shared.module.ts
+++ Angular/04-paisesApp/src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 
 
@@ -12,7 +15,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
