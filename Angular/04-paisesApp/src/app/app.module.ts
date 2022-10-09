446292ebd5db6654048a6e4d605f4ac3547beb9e
FranMV <FranManzanedoValle@gmail.com>
2022-10-09 17:19:21 +0200

    04-PaisesApp: Servicio para buscar paises

--- Angular/04-paisesApp/src/app/app.module.ts
+++ Angular/04-paisesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     PaisModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/04-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ Angular/04-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -10,9 +11,13 @@ export class PorPaisComponent {
 
   termino : string = ' ';
 
-  constructor() { }
+  constructor( private paisSevice: PaisService) { }
 
   buscar(){
     console.log(this.termino);
+    this.paisSevice.buscarPais(this.termino)
+                   .subscribe(resp =>{
+                    console.log(resp);
+                   });
   }
 }
